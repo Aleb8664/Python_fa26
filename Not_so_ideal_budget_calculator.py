51e2c1bf9d2a66f1b40996e322449b7185e07a76
Aleb <lexkusiak@gmail.com>
2026-09-01 13:50:37 -0500

    added to the program not ideal budget calc. added more inputs and comments

--- Not_so_ideal_budget_calculator.py
+++ Not_so_ideal_budget_calculator.py
@@ -11,9 +11,21 @@ ASSIGNMENT REQUIREMENTS
 -----------------------------------------------------------------------
 """
 
-print("Hello user, To begin I (the program) is going to ask a few questions.\n")
+# This is the introduction to the program, this lets the user know what they are doing!
+print("\n\nHello user, To begin I (the program) is going to ask a few questions.\n")
 
+
+# This section is the round of questioning for the user to find out how  much they make, their spending and the amount they would like to save
 user_name = input("Please enter your name:  ")
 print(f"thank you {user_name}!\n")
 income_monthly = float(input("Please State your monthly income:  "))
 print(f"{user_name}, You entered ${income_monthly:,.2f} as your monthly income!\n")
+rent_monthly = float(input("please enter your monthly rent:  "))
+print(f"{user_name} you entered ${rent_monthly:,.2f}!\n")
+electric_monthly = float(input("Please enter your monthly electric bill:  "))
+print(f"{user_name}, You entered ${electric_monthly:,.2f}!\n")
+
+
+# this section will be the calculatulatuibs
+
+# this will be the output
