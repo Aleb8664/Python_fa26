25d8aab0e09aacbedfee68556a0e086fac3e42f4
Aleb <lexkusiak@gmail.com>
2026-09-15 12:49:21 -0500

    Add initial structure and header docstring for input validation assignment

---
+++ Simple_ticket_system.py
@@ -0,0 +1,17 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 5A: INPUT VALIDATION
+-----------------------------------------------------------------------
+[X] 1. Header Docstring included.
+[ ] 2. All 5 inputs have 'while' loop validation.
+[ ] 3. The more tickets loop uses .upper() and correct Boolean logic.
+[ ] 4. Include a try and except statement around the entire program. Should have one defined
+       exception (probably value error) and a generic exception
+[ ] 5. Have pinned a variable in the WATCH window and took a screenshot.
+-----------------------------------------------------------------------
+"""
+
+more_tickets = True
+while more_tickets:
+hfhfhfhfh you like tickets?: ")).lower
+    if 
