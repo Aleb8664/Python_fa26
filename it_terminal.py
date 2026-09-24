b253e54cb9b3b513fe5ce12ae2637a3643018a6e
Aleb <lexkusiak@gmail.com>
2026-09-24 12:56:25 -0500

    Add initial implementation of the department security terminal with header docstring and user credentials

---
+++ it_terminal.py
@@ -0,0 +1,17 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 6B: THE DEPARTMENT SECURITY TERMINAL
+-----------------------------------------------------------------------
+[X] 1. Header Docstring included.
+[X] 2. Department constant defined in ALL_CAPS.
+[ ] 3. Username tuple and password list defined.
+[ ] 4. While loop runs interactively.
+[ ] 5. Try/except catches TypeError and tells user to email help desk.
+-----------------------------------------------------------------------
+"""
+
+DEPARTMENT = "SOC"
+# SOC stands for security operation center
+
+USERNAME = ("Kubek", "Frytek", "Zegarek", "Bober", "Bałwanek")
+password = ["I<3WATER", "Salty2211", "0utOft!me", "riverd@m22", "WinterCold123"]
