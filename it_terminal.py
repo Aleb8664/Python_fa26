f1a9ec295458766f3312951977601c28831b8071
Aleb <lexkusiak@gmail.com>
2026-09-24 13:46:21 -0500

    quick text

--- it_terminal.py
+++ it_terminal.py
@@ -5,7 +5,7 @@ ASSIGNMENT 6B: THE DEPARTMENT SECURITY TERMINAL
 [X] 1. Header Docstring included.
 [X] 2. Department constant defined in ALL_CAPS.
 [X] 3. Username tuple and password list defined.
-[ ] 4. While loop runs interactively.
+[X] 4. While loop runs interactively.
 [ ] 5. Try/except catches TypeError and tells user to email help desk.
 -----------------------------------------------------------------------
 """
